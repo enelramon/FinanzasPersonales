8b9e4d21587f43dc9b643d5eb77790a716a71ec2
angelrod25 <angelrguez25@gmail.com>
2015-01-28 22:36:12 -0400

    Subiendo mi Formulario

---
+++ FinanzasPersonales/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace FinanzasPersonales
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.IdTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NombreTextBox = new System.Windows.Forms.TextBox();
+            this.BuscarButton = new System.Windows.Forms.Button();
+            this.ActivaCheckBox = new System.Windows.Forms.CheckBox();
+            this.AceptarButton = new System.Windows.Forms.Button();
+            this.LimpiarButton = new System.Windows.Forms.Button();
+            this.EliminarButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Schoolbook", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(155, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID #:";
+            // 
+            // IdTextBox
+            // 
+            this.IdTextBox.Location = new System.Drawing.Point(219, 29);
+            this.IdTextBox.Name = "IdTextBox";
+            this.IdTextBox.Size = new System.Drawing.Size(100, 20);
+            this.IdTextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Schoolbook", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre de Clasificación:";
+            // 
+            // NombreTextBox
+            // 
+            this.NombreTextBox.Location = new System.Drawing.Point(219, 62);
+            this.NombreTextBox.Name = "NombreTextBox";
+            this.NombreTextBox.Size = new System.Drawing.Size(207, 20);
+            this.NombreTextBox.TabIndex = 3;
+            // 
+            // BuscarButton
+            // 
+            this.BuscarButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BuscarButton.BackgroundImage")));
+            this.BuscarButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.BuscarButton.Location = new System.Drawing.Point(342, 6);
+            this.BuscarButton.Name = "BuscarButton";
+            this.BuscarButton.Size = new System.Drawing.Size(59, 50);
+            this.BuscarButton.TabIndex = 4;
+            this.BuscarButton.UseVisualStyleBackColor = true;
+            // 
+            // ActivaCheckBox
+            // 
+            this.ActivaCheckBox.AutoSize = true;
+            this.ActivaCheckBox.Font = new System.Drawing.Font("Century Schoolbook", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ActivaCheckBox.Location = new System.Drawing.Point(218, 106);
+            this.ActivaCheckBox.Name = "ActivaCheckBox";
+            this.ActivaCheckBox.Size = new System.Drawing.Size(72, 22);
+            this.ActivaCheckBox.TabIndex = 5;
+            this.ActivaCheckBox.Text = "Activa";
+            this.ActivaCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // AceptarButton
+            // 
+            this.AceptarButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("AceptarButton.BackgroundImage")));
+            this.AceptarButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.AceptarButton.Location = new System.Drawing.Point(103, 152);
+            this.AceptarButton.Name = "AceptarButton";
+            this.AceptarButton.Size = new System.Drawing.Size(60, 60);
+            this.AceptarButton.TabIndex = 6;
+            this.AceptarButton.UseVisualStyleBackColor = true;
+            // 
+            // LimpiarButton
+            // 
+            this.LimpiarButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("LimpiarButton.BackgroundImage")));
+            this.LimpiarButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.LimpiarButton.Location = new System.Drawing.Point(234, 150);
+            this.LimpiarButton.Name = "LimpiarButton";
+            this.LimpiarButton.Size = new System.Drawing.Size(69, 62);
+            this.LimpiarButton.TabIndex = 7;
+            this.LimpiarButton.UseVisualStyleBackColor = true;
+            // 
+            // EliminarButton
+            // 
+            this.EliminarButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("EliminarButton.BackgroundImage")));
+            this.EliminarButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.EliminarButton.Location = new System.Drawing.Point(361, 150);
+            this.EliminarButton.Name = "EliminarButton";
+            this.EliminarButton.Size = new System.Drawing.Size(65, 60);
+            this.EliminarButton.TabIndex = 8;
+            this.EliminarButton.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(109, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Aceptar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(250, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Limpiar";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(371, 217);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Eliminar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 247);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.EliminarButton);
+            this.Controls.Add(this.LimpiarButton);
+            this.Controls.Add(this.AceptarButton);
+            this.Controls.Add(this.ActivaCheckBox);
+            this.Controls.Add(this.BuscarButton);
+            this.Controls.Add(this.NombreTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.IdTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Registro de Clasificaciones - Versión Prueba";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox IdTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NombreTextBox;
+        private System.Windows.Forms.Button BuscarButton;
+        private System.Windows.Forms.CheckBox ActivaCheckBox;
+        private System.Windows.Forms.Button AceptarButton;
+        private System.Windows.Forms.Button LimpiarButton;
+        private System.Windows.Forms.Button EliminarButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
