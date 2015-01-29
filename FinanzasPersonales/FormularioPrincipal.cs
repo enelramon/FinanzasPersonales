5f7be759add4e520f65f369c7e2e3dd7b1b691a7
YahaidaVargas <lafenix2003@gmail.com>
2015-01-29 16:00:10 -0400

    creando mi primer ventana con git

--- FinanzasPersonales/FormularioPrincipal.cs
+++ FinanzasPersonales/FormularioPrincipal.cs
@@ -31,5 +31,20 @@ namespace FinanzasPersonales
         {
 
         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
