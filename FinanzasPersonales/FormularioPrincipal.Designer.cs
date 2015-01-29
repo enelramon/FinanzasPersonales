1373f9541270c3d33e99d460d3a4ee942bc23bdb
chrisgenao <christopher_genao@hotmail.com>
2015-01-29 16:50:31 -0400

    Cambiando algunas cositas

--- FinanzasPersonales/FormularioPrincipal.Designer.cs
+++ FinanzasPersonales/FormularioPrincipal.Designer.cs
@@ -34,10 +34,10 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.BuscarButtom = new System.Windows.Forms.Button();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.AceptarButtom = new System.Windows.Forms.Button();
             this.BottarButtom = new System.Windows.Forms.Button();
             this.LimpiarButtom = new System.Windows.Forms.Button();
+            this.Active = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // IDTextBox
@@ -58,40 +58,32 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(46, 15);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.Size = new System.Drawing.Size(25, 16);
             this.label1.TabIndex = 2;
             this.label1.Text = "ID:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(20, 41);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.Size = new System.Drawing.Size(64, 16);
             this.label2.TabIndex = 3;
             this.label2.Text = "Nombre:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // BuscarButtom
             // 
             this.BuscarButtom.Image = ((System.Drawing.Image)(resources.GetObject("BuscarButtom.Image")));
             this.BuscarButtom.Location = new System.Drawing.Point(263, 8);
             this.BuscarButtom.Name = "BuscarButtom";
-            this.BuscarButtom.Size = new System.Drawing.Size(27, 26);
+            this.BuscarButtom.Size = new System.Drawing.Size(28, 26);
             this.BuscarButtom.TabIndex = 4;
             this.BuscarButtom.UseVisualStyleBackColor = true;
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(12, 93);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(55, 17);
-            this.radioButton1.TabIndex = 5;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Activa";
-            this.radioButton1.UseVisualStyleBackColor = true;
             // 
             // AceptarButtom
             // 
@@ -132,6 +124,18 @@
             this.LimpiarButtom.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.LimpiarButtom.UseVisualStyleBackColor = true;
             // 
+            // Active
+            // 
+            this.Active.AutoSize = true;
+            this.Active.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Active.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Active.Location = new System.Drawing.Point(12, 97);
+            this.Active.Name = "Active";
+            this.Active.Size = new System.Drawing.Size(68, 20);
+            this.Active.TabIndex = 9;
+            this.Active.Text = "Active";
+            this.Active.UseVisualStyleBackColor = true;
+            // 
             // FormularioPrincipal
             // 
             this.AcceptButton = this.AceptarButtom;
@@ -140,10 +144,10 @@
             this.BackColor = System.Drawing.SystemColors.Control;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(319, 155);
+            this.Controls.Add(this.Active);
             this.Controls.Add(this.LimpiarButtom);
             this.Controls.Add(this.BottarButtom);
             this.Controls.Add(this.AceptarButtom);
-            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.BuscarButtom);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -151,6 +155,8 @@
             this.Controls.Add(this.IDTextBox);
             this.Cursor = System.Windows.Forms.Cursors.Default;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "FormularioPrincipal";
             this.Text = "Registro de Clasificaciones";
             this.Load += new System.EventHandler(this.FormularioPrincipal_Load);
@@ -166,10 +172,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button BuscarButtom;
-        private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.Button AceptarButtom;
         private System.Windows.Forms.Button BottarButtom;
         private System.Windows.Forms.Button LimpiarButtom;
+        private System.Windows.Forms.CheckBox Active;
     }
 }
 
