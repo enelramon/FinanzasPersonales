4da2338eadfe12d90081844aa667ffe6f547f004
YahaidaVargas <lafenix2003@gmail.com>
2015-01-29 21:07:39 -0400

    ok ya esta es la ultima, jejej

--- FinanzasPersonales/FormularioPrincipal.Designer.cs
+++ FinanzasPersonales/FormularioPrincipal.Designer.cs
@@ -46,14 +46,14 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(86, 30);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(126, 20);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(86, 81);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(199, 20);
+            this.textBox2.Size = new System.Drawing.Size(232, 20);
             this.textBox2.TabIndex = 1;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
@@ -76,10 +76,10 @@
             this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button1.Location = new System.Drawing.Point(34, 180);
+            this.button1.Location = new System.Drawing.Point(34, 190);
             this.button1.Name = "button1";
             this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.button1.Size = new System.Drawing.Size(97, 42);
+            this.button1.Size = new System.Drawing.Size(112, 42);
             this.button1.TabIndex = 3;
             this.button1.Text = "Aceptar";
             this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -95,9 +95,9 @@
             this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
             this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
             this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button2.Location = new System.Drawing.Point(137, 180);
+            this.button2.Location = new System.Drawing.Point(168, 189);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(105, 43);
+            this.button2.Size = new System.Drawing.Size(126, 43);
             this.button2.TabIndex = 4;
             this.button2.Text = "Limpiar";
             this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -112,9 +112,9 @@
             this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
             this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
             this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button3.Location = new System.Drawing.Point(248, 180);
+            this.button3.Location = new System.Drawing.Point(310, 189);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(96, 43);
+            this.button3.Size = new System.Drawing.Size(110, 43);
             this.button3.TabIndex = 5;
             this.button3.Text = "Borrar";
             this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -126,7 +126,7 @@
             this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
-            this.button4.Location = new System.Drawing.Point(205, 20);
+            this.button4.Location = new System.Drawing.Point(228, 20);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(32, 30);
             this.button4.TabIndex = 6;
@@ -156,7 +156,7 @@
             this.groupBox1.Controls.Add(this.textBox2);
             this.groupBox1.Location = new System.Drawing.Point(34, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(310, 160);
+            this.groupBox1.Size = new System.Drawing.Size(386, 162);
             this.groupBox1.TabIndex = 9;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Registro";
@@ -167,7 +167,7 @@
             this.checkBox1.AutoSize = true;
             this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.checkBox1.ForeColor = System.Drawing.Color.Maroon;
-            this.checkBox1.Location = new System.Drawing.Point(20, 122);
+            this.checkBox1.Location = new System.Drawing.Point(20, 119);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(71, 21);
             this.checkBox1.TabIndex = 8;
@@ -182,7 +182,7 @@
             this.BackColor = System.Drawing.Color.Cyan;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(397, 260);
+            this.ClientSize = new System.Drawing.Size(473, 289);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
