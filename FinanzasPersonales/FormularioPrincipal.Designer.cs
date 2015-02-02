9ecff9aee8014f2307c660938debeb4b9b467e5a
chrisgenao <christopher_genao@hotmail.com>
2015-02-02 19:30:44 -0400

    Revert "Fondo de pantalla parcialmente eliminado."
    This reverts commit 873aa3ea9393b3fd5b5ae25a52a9073d8d83a4ea.

--- FinanzasPersonales/FormularioPrincipal.Designer.cs
+++ FinanzasPersonales/FormularioPrincipal.Designer.cs
@@ -42,69 +42,108 @@
             // 
             // IDTextBox
             // 
-            resources.ApplyResources(this.IDTextBox, "IDTextBox");
+            this.IDTextBox.Location = new System.Drawing.Point(134, 12);
             this.IDTextBox.Name = "IDTextBox";
+            this.IDTextBox.Size = new System.Drawing.Size(100, 20);
+            this.IDTextBox.TabIndex = 0;
             this.IDTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // NombreTextBox
             // 
-            resources.ApplyResources(this.NombreTextBox, "NombreTextBox");
+            this.NombreTextBox.Location = new System.Drawing.Point(134, 38);
             this.NombreTextBox.Name = "NombreTextBox";
+            this.NombreTextBox.Size = new System.Drawing.Size(173, 20);
+            this.NombreTextBox.TabIndex = 1;
             // 
             // label1
             // 
-            resources.ApplyResources(this.label1, "label1");
-            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(46, 15);
             this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID:";
             // 
             // label2
             // 
-            resources.ApplyResources(this.label2, "label2");
-            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 41);
             this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombre:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // BuscarButtom
             // 
-            resources.ApplyResources(this.BuscarButtom, "BuscarButtom");
+            this.BuscarButtom.Image = ((System.Drawing.Image)(resources.GetObject("BuscarButtom.Image")));
+            this.BuscarButtom.Location = new System.Drawing.Point(263, 8);
             this.BuscarButtom.Name = "BuscarButtom";
+            this.BuscarButtom.Size = new System.Drawing.Size(28, 26);
+            this.BuscarButtom.TabIndex = 4;
             this.BuscarButtom.UseVisualStyleBackColor = true;
             // 
             // AceptarButtom
             // 
-            resources.ApplyResources(this.AceptarButtom, "AceptarButtom");
             this.AceptarButtom.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.AceptarButtom.Image = ((System.Drawing.Image)(resources.GetObject("AceptarButtom.Image")));
+            this.AceptarButtom.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.AceptarButtom.Location = new System.Drawing.Point(232, 64);
             this.AceptarButtom.Name = "AceptarButtom";
+            this.AceptarButtom.Size = new System.Drawing.Size(75, 23);
+            this.AceptarButtom.TabIndex = 6;
+            this.AceptarButtom.Text = "Aceptar";
+            this.AceptarButtom.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.AceptarButtom.UseVisualStyleBackColor = true;
             this.AceptarButtom.Click += new System.EventHandler(this.button2_Click);
             // 
             // BottarButtom
             // 
-            resources.ApplyResources(this.BottarButtom, "BottarButtom");
             this.BottarButtom.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.BottarButtom.Image = ((System.Drawing.Image)(resources.GetObject("BottarButtom.Image")));
+            this.BottarButtom.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BottarButtom.Location = new System.Drawing.Point(232, 122);
             this.BottarButtom.Name = "BottarButtom";
+            this.BottarButtom.Size = new System.Drawing.Size(75, 23);
+            this.BottarButtom.TabIndex = 7;
+            this.BottarButtom.Text = "Borrar";
+            this.BottarButtom.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.BottarButtom.UseVisualStyleBackColor = true;
             // 
             // LimpiarButtom
             // 
-            resources.ApplyResources(this.LimpiarButtom, "LimpiarButtom");
+            this.LimpiarButtom.Image = ((System.Drawing.Image)(resources.GetObject("LimpiarButtom.Image")));
+            this.LimpiarButtom.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.LimpiarButtom.Location = new System.Drawing.Point(232, 93);
             this.LimpiarButtom.Name = "LimpiarButtom";
+            this.LimpiarButtom.Size = new System.Drawing.Size(75, 23);
+            this.LimpiarButtom.TabIndex = 8;
+            this.LimpiarButtom.Text = "Limpiar";
+            this.LimpiarButtom.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.LimpiarButtom.UseVisualStyleBackColor = true;
             // 
             // Active
             // 
-            resources.ApplyResources(this.Active, "Active");
-            this.Active.BackColor = System.Drawing.Color.Transparent;
+            this.Active.AutoSize = true;
+            this.Active.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Active.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Active.Location = new System.Drawing.Point(12, 97);
             this.Active.Name = "Active";
-            this.Active.UseVisualStyleBackColor = false;
+            this.Active.Size = new System.Drawing.Size(68, 20);
+            this.Active.TabIndex = 9;
+            this.Active.Text = "Active";
+            this.Active.UseVisualStyleBackColor = true;
             // 
             // FormularioPrincipal
             // 
             this.AcceptButton = this.AceptarButtom;
-            resources.ApplyResources(this, "$this");
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(319, 155);
             this.Controls.Add(this.Active);
             this.Controls.Add(this.LimpiarButtom);
             this.Controls.Add(this.BottarButtom);
@@ -115,9 +154,11 @@
             this.Controls.Add(this.NombreTextBox);
             this.Controls.Add(this.IDTextBox);
             this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FormularioPrincipal";
+            this.Text = "Registro de Clasificaciones";
             this.Load += new System.EventHandler(this.FormularioPrincipal_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
