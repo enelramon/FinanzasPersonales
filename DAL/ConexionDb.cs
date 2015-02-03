67b7154fd8e94b1a3d519f35d4a879451c6383f1
Enel Almonte <enelramon@gmail.com>
2015-02-02 20:54:31 -0400

    Creando la clase ConexionDb

---
+++ DAL/ConexionDb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    /// <summary>
+    /// Clase para gestionar la conexion a la base de datos SQL SERVER
+    /// </summary>
+    class ConexionDb
+    {
+    }
+}
