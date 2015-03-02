9b56495bf018dda9fa468cd631832b7665017410
chrisgenao <christopher_genao@hotmail.com>
2015-03-02 19:56:00 -0400

    Lista

--- FinanzasPersonales/Registros/rGastos.Designer.cs
+++ FinanzasPersonales/Registros/rGastos.Designer.cs
@@ -103,9 +103,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 15);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.Size = new System.Drawing.Size(66, 13);
             this.label1.TabIndex = 8;
-            this.label1.Text = "IdGasto";
+            this.label1.Text = "ID del Gasto";
             // 
             // label3
             // 
