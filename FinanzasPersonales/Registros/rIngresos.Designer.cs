633c30fa4ba637964fb3ad3840920d909388ee28
YahaidaVargas <lafenix2003@gmail.com>
2015-03-09 17:55:36 -0430

    yahaidaotravez

---
+++ FinanzasPersonales/Registros/rIngresos.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace FinanzasPersonales.Registros
+{
+    partial class rIngresos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(rIngresos));
+            this.LimpiarButtom = new System.Windows.Forms.Button();
+            this.BorrarButtom = new System.Windows.Forms.Button();
+            this.GuardarButtom = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1Ingreso = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BuscarButtom = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2IdIngreso = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2Concepto = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LimpiarButtom
+            // 
+            this.LimpiarButtom.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.LimpiarButtom.Image = ((System.Drawing.Image)(resources.GetObject("LimpiarButtom.Image")));
+            this.LimpiarButtom.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.LimpiarButtom.Location = new System.Drawing.Point(17, 182);
+            this.LimpiarButtom.Name = "LimpiarButtom";
+            this.LimpiarButtom.Size = new System.Drawing.Size(75, 33);
+            this.LimpiarButtom.TabIndex = 61;
+            this.LimpiarButtom.Text = "Limpiar";
+            this.LimpiarButtom.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.LimpiarButtom.UseVisualStyleBackColor = true;
+            this.LimpiarButtom.Click += new System.EventHandler(this.LimpiarButtom_Click);
+            // 
+            // BorrarButtom
+            // 
+            this.BorrarButtom.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.BorrarButtom.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.BorrarButtom.Image = ((System.Drawing.Image)(resources.GetObject("BorrarButtom.Image")));
+            this.BorrarButtom.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BorrarButtom.Location = new System.Drawing.Point(212, 182);
+            this.BorrarButtom.Name = "BorrarButtom";
+            this.BorrarButtom.Size = new System.Drawing.Size(75, 33);
+            this.BorrarButtom.TabIndex = 60;
+            this.BorrarButtom.Text = "Borrar";
+            this.BorrarButtom.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BorrarButtom.UseVisualStyleBackColor = true;
+            this.BorrarButtom.Click += new System.EventHandler(this.BorrarButtom_Click);
+            // 
+            // GuardarButtom
+            // 
+            this.GuardarButtom.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.GuardarButtom.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.GuardarButtom.Image = ((System.Drawing.Image)(resources.GetObject("GuardarButtom.Image")));
+            this.GuardarButtom.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.GuardarButtom.Location = new System.Drawing.Point(112, 182);
+            this.GuardarButtom.Name = "GuardarButtom";
+            this.GuardarButtom.Size = new System.Drawing.Size(75, 33);
+            this.GuardarButtom.TabIndex = 59;
+            this.GuardarButtom.Text = "Guardar";
+            this.GuardarButtom.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.GuardarButtom.UseVisualStyleBackColor = true;
+            this.GuardarButtom.Click += new System.EventHandler(this.GuardarButtom_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Fecha de ingreso";
+            // 
+            // dateTimePicker1Ingreso
+            // 
+            this.dateTimePicker1Ingreso.Location = new System.Drawing.Point(127, 28);
+            this.dateTimePicker1Ingreso.Name = "dateTimePicker1Ingreso";
+            this.dateTimePicker1Ingreso.Size = new System.Drawing.Size(175, 20);
+            this.dateTimePicker1Ingreso.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Por concepto de";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(127, 115);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(175, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Valor del ingreso";
+            // 
+            // BuscarButtom
+            // 
+            this.BuscarButtom.Image = ((System.Drawing.Image)(resources.GetObject("BuscarButtom.Image")));
+            this.BuscarButtom.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BuscarButtom.Location = new System.Drawing.Point(291, 58);
+            this.BuscarButtom.Name = "BuscarButtom";
+            this.BuscarButtom.Size = new System.Drawing.Size(82, 34);
+            this.BuscarButtom.TabIndex = 73;
+            this.BuscarButtom.Text = "Buscar";
+            this.BuscarButtom.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BuscarButtom.UseVisualStyleBackColor = true;
+            this.BuscarButtom.Click += new System.EventHandler(this.BuscarButtom_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2Concepto);
+            this.groupBox1.Controls.Add(this.LimpiarButtom);
+            this.groupBox1.Controls.Add(this.BorrarButtom);
+            this.groupBox1.Controls.Add(this.GuardarButtom);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.dateTimePicker1Ingreso);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(71, 98);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(350, 238);
+            this.groupBox1.TabIndex = 72;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // textBox2IdIngreso
+            // 
+            this.textBox2IdIngreso.Location = new System.Drawing.Point(164, 66);
+            this.textBox2IdIngreso.Name = "textBox2IdIngreso";
+            this.textBox2IdIngreso.Size = new System.Drawing.Size(118, 20);
+            this.textBox2IdIngreso.TabIndex = 74;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(89, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 75;
+            this.label4.Text = "ID Ingreso";
+            // 
+            // textBox2Concepto
+            // 
+            this.textBox2Concepto.Location = new System.Drawing.Point(127, 75);
+            this.textBox2Concepto.Name = "textBox2Concepto";
+            this.textBox2Concepto.Size = new System.Drawing.Size(175, 20);
+            this.textBox2Concepto.TabIndex = 62;
+            // 
+            // rIngresos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 375);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2IdIngreso);
+            this.Controls.Add(this.BuscarButtom);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "rIngresos";
+            this.Text = "rIngresos";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void BorrarButtom_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LimpiarButtom;
+        private System.Windows.Forms.Button BorrarButtom;
+        private System.Windows.Forms.Button GuardarButtom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1Ingreso;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button BuscarButtom;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2IdIngreso;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2Concepto;
+
+
+    }
+}
