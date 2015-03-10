7692fef9861a03ecffba561a6a3630f74d959682
chrisgenao <christopher_genao@hotmail.com>
2015-03-09 22:08:41 -0400

    Combinando PC con Laptop

---
+++ FinanzasPersonales/Registros/rCuentasXCobrar.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace FinanzasPersonales.Registros
+{
+    partial class rCuentasXCobrar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(rCuentasXCobrar));
+            this.label3 = new System.Windows.Forms.Label();
+            this.LimpiarButtom = new System.Windows.Forms.Button();
+            this.BorrarButtom = new System.Windows.Forms.Button();
+            this.GuardarButtom = new System.Windows.Forms.Button();
+            this.BuscarButtom = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.IdPrestamoTextBox = new System.Windows.Forms.TextBox();
+            this.CuentaComboBox = new System.Windows.Forms.ComboBox();
+            this.ConceptoTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.MontoTextBox = new System.Windows.Forms.TextBox();
+            this.BalancePTextBox3 = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider3 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 39;
+            this.label3.Text = "Concepto";
+            // 
+            // LimpiarButtom
+            // 
+            this.LimpiarButtom.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.LimpiarButtom.Image = ((System.Drawing.Image)(resources.GetObject("LimpiarButtom.Image")));
+            this.LimpiarButtom.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.LimpiarButtom.Location = new System.Drawing.Point(97, 217);
+            this.LimpiarButtom.Name = "LimpiarButtom";
+            this.LimpiarButtom.Size = new System.Drawing.Size(75, 33);
+            this.LimpiarButtom.TabIndex = 37;
+            this.LimpiarButtom.Text = "Limpiar";
+            this.LimpiarButtom.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.LimpiarButtom.UseVisualStyleBackColor = true;
+            this.LimpiarButtom.Click += new System.EventHandler(this.LimpiarButtom_Click);
+            // 
+            // BorrarButtom
+            // 
+            this.BorrarButtom.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.BorrarButtom.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.BorrarButtom.Image = ((System.Drawing.Image)(resources.GetObject("BorrarButtom.Image")));
+            this.BorrarButtom.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BorrarButtom.Location = new System.Drawing.Point(259, 217);
+            this.BorrarButtom.Name = "BorrarButtom";
+            this.BorrarButtom.Size = new System.Drawing.Size(75, 33);
+            this.BorrarButtom.TabIndex = 36;
+            this.BorrarButtom.Text = "Borrar";
+            this.BorrarButtom.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BorrarButtom.UseVisualStyleBackColor = true;
+            this.BorrarButtom.Click += new System.EventHandler(this.BorrarButtom_Click);
+            // 
+            // GuardarButtom
+            // 
+            this.GuardarButtom.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.GuardarButtom.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.GuardarButtom.Image = ((System.Drawing.Image)(resources.GetObject("GuardarButtom.Image")));
+            this.GuardarButtom.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.GuardarButtom.Location = new System.Drawing.Point(178, 217);
+            this.GuardarButtom.Name = "GuardarButtom";
+            this.GuardarButtom.Size = new System.Drawing.Size(75, 33);
+            this.GuardarButtom.TabIndex = 35;
+            this.GuardarButtom.Text = "Guardar";
+            this.GuardarButtom.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.GuardarButtom.UseVisualStyleBackColor = true;
+            this.GuardarButtom.Click += new System.EventHandler(this.GuardarButtom_Click);
+            // 
+            // BuscarButtom
+            // 
+            this.BuscarButtom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.BuscarButtom.Image = ((System.Drawing.Image)(resources.GetObject("BuscarButtom.Image")));
+            this.BuscarButtom.Location = new System.Drawing.Point(178, 26);
+            this.BuscarButtom.Name = "BuscarButtom";
+            this.BuscarButtom.Size = new System.Drawing.Size(34, 24);
+            this.BuscarButtom.TabIndex = 34;
+            this.BuscarButtom.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "Cuenta";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 33;
+            this.label1.Text = "ID";
+            // 
+            // IdPrestamoTextBox
+            // 
+            this.IdPrestamoTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.IdPrestamoTextBox.Location = new System.Drawing.Point(83, 29);
+            this.IdPrestamoTextBox.Name = "IdPrestamoTextBox";
+            this.IdPrestamoTextBox.Size = new System.Drawing.Size(77, 20);
+            this.IdPrestamoTextBox.TabIndex = 30;
+            this.IdPrestamoTextBox.TextChanged += new System.EventHandler(this.IdPrestamoTextBox_TextChanged);
+            // 
+            // CuentaComboBox
+            // 
+            this.CuentaComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CuentaComboBox.FormattingEnabled = true;
+            this.CuentaComboBox.Items.AddRange(new object[] {
+            "IdCuenta",
+            "Concepto"});
+            this.CuentaComboBox.Location = new System.Drawing.Point(83, 64);
+            this.CuentaComboBox.Name = "CuentaComboBox";
+            this.CuentaComboBox.Size = new System.Drawing.Size(310, 21);
+            this.CuentaComboBox.TabIndex = 40;
+            // 
+            // ConceptoTextBox
+            // 
+            this.ConceptoTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ConceptoTextBox.Location = new System.Drawing.Point(83, 93);
+            this.ConceptoTextBox.MaxLength = 50;
+            this.ConceptoTextBox.Name = "ConceptoTextBox";
+            this.ConceptoTextBox.Size = new System.Drawing.Size(310, 20);
+            this.ConceptoTextBox.TabIndex = 41;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 42;
+            this.label4.Text = "Monto";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(209, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 13);
+            this.label5.TabIndex = 43;
+            this.label5.Text = "Balance Pendiente:";
+            // 
+            // MontoTextBox
+            // 
+            this.MontoTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.MontoTextBox.Location = new System.Drawing.Point(61, 182);
+            this.MontoTextBox.Name = "MontoTextBox";
+            this.MontoTextBox.Size = new System.Drawing.Size(99, 20);
+            this.MontoTextBox.TabIndex = 44;
+            // 
+            // BalancePTextBox3
+            // 
+            this.BalancePTextBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.BalancePTextBox3.Location = new System.Drawing.Point(315, 182);
+            this.BalancePTextBox3.Name = "BalancePTextBox3";
+            this.BalancePTextBox3.ReadOnly = true;
+            this.BalancePTextBox3.Size = new System.Drawing.Size(100, 20);
+            this.BalancePTextBox3.TabIndex = 45;
+            this.BalancePTextBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // errorProvider3
+            // 
+            this.errorProvider3.ContainerControl = this;
+            // 
+            // rCuentasXCobrar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 262);
+            this.Controls.Add(this.BalancePTextBox3);
+            this.Controls.Add(this.MontoTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ConceptoTextBox);
+            this.Controls.Add(this.CuentaComboBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LimpiarButtom);
+            this.Controls.Add(this.BorrarButtom);
+            this.Controls.Add(this.GuardarButtom);
+            this.Controls.Add(this.BuscarButtom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.IdPrestamoTextBox);
+            this.Name = "rCuentasXCobrar";
+            this.Text = "Registros de Prestamos";
+            this.Load += new System.EventHandler(this.rCuentasXCobrar_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button LimpiarButtom;
+        private System.Windows.Forms.Button BorrarButtom;
+        private System.Windows.Forms.Button GuardarButtom;
+        private System.Windows.Forms.Button BuscarButtom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox IdPrestamoTextBox;
+        private System.Windows.Forms.ComboBox CuentaComboBox;
+        private System.Windows.Forms.TextBox ConceptoTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox MontoTextBox;
+        private System.Windows.Forms.TextBox BalancePTextBox3;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+        private System.Windows.Forms.ErrorProvider errorProvider3;
+    }
+}
